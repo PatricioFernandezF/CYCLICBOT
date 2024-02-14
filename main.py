04ce952842690c9ea893fef8ab34315799772117
PatricioFernandezF <patricio.fernandez.florez@gmail.com>
2024-02-14 23:53:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,6 @@ WORKFLOW=os.getenv('workflow')
 webhook_url = os.getenv('CYCLIC_URL', 'http://localhost:8181') + "/webhook/"
 
 
-
-
 bot = Bot(token=bot_token)
 asyncio.run(setup_webhook())
 
@@ -52,9 +50,33 @@ async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_teleg
     if text == "/start":
         with open('hello.gif', 'rb') as photo:
             await bot.send_photo(chat_id=chat_id, photo=photo)
-        await bot.send_message(chat_id=chat_id, text="Welcome to Cyclic Starter Python Telegram Bot!")
+        await bot.send_message(chat_id=update.effective_chat.id, text="¡Hola! Soy un bot de Telegram. Pon /prompt + el prompt")
     else:
-        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Yo!")
+        if text!="/prompt":
+            await bot.send_message(chat_id=update.effective_chat.id, text="Procesando Prompt: "+text)
+            api_key = TOKEN
+            comfy_api = ComfyDeployAPI(api_key)
+
+            # Ejemplo de cómo desplegar un workflow
+            workflow_id = WORKFLOW
+            run_response = comfy_api.run_workflow(workflow_id,{"input_text":text})
+            print(run_response)
+
+            # Ejemplo de cómo obtener la salida de la ejecución de un workflow
+            run_id = run_response["run_id"] # Reemplaza con el run_id real obtenido después de ejecutar el workflow
+            if run_id:
+                output_response = comfy_api.get_workflow_run_output(run_id)
+                print(output_response)
+
+                image_info = output_response.get('outputs', [{}])[0].get('data', {}).get('images', [{}])[0]
+                image_url = image_info.get('url')
+
+                if image_url:
+                    await bot.send_photo(chat_id=update.effective_chat.id, photo=image_url)
+                    return  # Finaliza la función después de enviar la imagen
+
+        else:
+            await bot.send_message(chat_id=update.effective_chat.id, text="Recuerda que solo estoy programado para recibir /prompt")
 
     return {"ok": True}
 
