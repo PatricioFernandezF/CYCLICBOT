61f139e589164849027a18a2a7aa3f2d13d501c9
PatricioFernandezF <patricio.fernandez.florez@gmail.com>
2024-02-14 23:12:01 +0100

    CAMBIADA VARIABLE

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 load_dotenv()
 
 # Read the variable from the environment (or .env file)
-bot_token = os.getenv('telegram')
+bot_token = os.getenv('BOT_TOKEN')
 secret_token = os.getenv("SECRET_TOKEN")
 TOKEN = os.getenv('comfyapi')
 WORKFLOW=os.getenv('workflow')
