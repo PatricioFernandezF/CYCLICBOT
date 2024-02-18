dbedcccd1331707cca85efcf28b75b4646a0e504
PatricioFernandezF <patricio.fernandez.florez@gmail.com>
2024-02-18 21:20:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -57,36 +57,8 @@ async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_teleg
             await bot.send_photo(chat_id=chat_id, photo=photo)
         await bot.send_message(chat_id=chat_id, text="¡Hola! Soy un bot de Telegram. Pon /prompt + el prompt")
     else:
-        if "/prompt" in text:
-            prompt=text.replace("/prompt ","")
-            await bot.send_message(chat_id=chat_id, text="Procesando Prompt: "+prompt)
-            api_key = TOKEN
-            comfy_api = ComfyDeployAPI(api_key)
-            workflow_id = WORKFLOW
-            run_response = await comfy_api.run_workflow(workflow_id,{"input_text":prompt})
-            print(run_response)
 
-            run_id = run_response["run_id"] # Reemplaza con el run_id real obtenido después de ejecutar el workflow
-    
-            try:
-                if run_id:
-                    output_response = await comfy_api.get_workflow_run_output(run_id)
-                    print(output_response)
-                    
-                    image_info = output_response.get('outputs', [{}])[0].get('data', {}).get('images', [{}])[0]
-                    image_url = image_info.get('url')
-                
-                    if image_url:
-                        await bot.send_photo(chat_id=chat_id, photo=image_url)
-                    else:
-                        await bot.send_message(chat_id=chat_id, text="Ha habido un error con el prompt")
-                        return {"ok": True}
-            except:
-                await bot.send_message(chat_id=chat_id, text="Ha habido un error con el prompt")
-                return {"ok": True}  
-                
-        else:
-            await bot.send_message(chat_id=chat_id, text="Recuerda que solo estoy programado para recibir /prompt")
+        await bot.send_message(chat_id=chat_id, text="Recuerda que solo estoy programado para recibir /prompt")
 
     return {"ok": True}
 
